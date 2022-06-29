10a620132dd5fd58b2b82d00b0487e9e5f18a6ca
irisu-inwl <iris.dimension.eee@gmail.com>
2022-06-29 20:54:55 +0900

    fix ast

--- src/ast/ast.go
+++ src/ast/ast.go
@@ -26,11 +26,6 @@ type LetStatement struct {
 	Value Expression
 }
 
-type ReturnStatement struct {
-	Token       token.Token // token.RETURN
-	ReturnValue Expression
-}
-
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
@@ -41,6 +36,11 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+type ReturnStatement struct {
+	Token       token.Token // token.RETURN
+	ReturnValue Expression
+}
 
 func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
